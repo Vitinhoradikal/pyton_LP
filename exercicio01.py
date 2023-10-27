6f8c071bc4293f2f7aef8adb22d177c81f846c03
Vitor Lucio Machado <vitorluciomachado@yahoo.com.br>
2023-10-27 11:18:16 -0300

    Criado o primeiro exercício de LP

---
+++ exercicio01.py
@@ -0,0 +1,12 @@
+print ("IMPRIME A IDADE EM ANOS, MESES E DIAS")
+
+idade = int(input("Digite a sua idade em dias."))
+
+anos = idade / 365
+meses = anos * 12
+
+print("Você já viveu por:")
+
+print(anos, "anos.")
+print(meses, "Meses")
+print(idade, "Dias")
